5ec5ca123e84b034f3705d08f6a8365a51ca944a
Mihir-21 <trambadiyamihir635@gmail.com>
2024-07-09 15:05:29 +0200

    Create 09_SUM.java

---
+++ Beginner/09_SUM.java
@@ -0,0 +1,12 @@
+// get 2 values from the user and print the sum  
+
+import java.util.Scanner;
+ public class sum {
+    public static void main (String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int sum = a + b; 
+        System.out.println(sum);
+    }
+ }
